98dd76ba6fffb1a456ff00a3d78d8758127e3463
Steve <15229911+stevehanstudio@users.noreply.github.com>
2021-07-17 17:42:56 -0700

    Comment out code in Project SEO

--- components/ProjectSeo.js
+++ components/ProjectSeo.js
@@ -1,44 +1,44 @@
-import { NextSeo, ArticleJsonLd } from 'next-seo';
-import { format } from '../lib/dateUtils'
+// import { NextSeo, ArticleJsonLd } from 'next-seo';
+// import { format } from '../lib/dateUtils'
 
-const ProjectSeo = ({ title, subtitle, d, image }) => {
-	const date = format(d);
-	const url = 'https://stevehan.dev'
-	const featuredImage = {
-		url: `./${image}`,
-		alt: title,
-	};
+// const ProjectSeo = ({ title, subtitle, d, image }) => {
+// 	const date = format(d);
+// 	const url = 'https://stevehan.dev'
+// 	const featuredImage = {
+// 		url: `./${image}`,
+// 		alt: title,
+// 	};
 
-	return (
-		<>
-			<NextSeo
-				title={`${title} – Steve Han`}
-				description={subtitle}
-				canonical={url}
-				openGraph={{
-					type: 'article',
-					article: {
-						publishedTime: date,
-					},
-					url,
-					title,
-					description: subtitle,
-					images: [featuredImage],
-				}}
-			/>
-			<ArticleJsonLd
-				authorName='Steve Han'
-				dateModified={date}
-				datePublished={date}
-				description={subtitle}
-				images={[featuredImage]}
-				publisherLogo='/static/favicons/icon-192x192.png'
-				publisherName='Steve Han'
-				title={title}
-				// url={url}
-			/>
-		</>
-	);
-};
+// 	return (
+// 		<>
+// 			<NextSeo
+// 				title={`${title} – Steve Han`}
+// 				description={subtitle}
+// 				canonical={url}
+// 				openGraph={{
+// 					type: 'article',
+// 					article: {
+// 						publishedTime: date,
+// 					},
+// 					url,
+// 					title,
+// 					description: subtitle,
+// 					images: [featuredImage],
+// 				}}
+// 			/>
+// 			<ArticleJsonLd
+// 				authorName='Steve Han'
+// 				dateModified={date}
+// 				datePublished={date}
+// 				description={subtitle}
+// 				images={[featuredImage]}
+// 				publisherLogo='/static/favicons/icon-192x192.png'
+// 				publisherName='Steve Han'
+// 				title={title}
+// 				// url={url}
+// 			/>
+// 		</>
+// 	);
+// };
 
-export default ProjectSeo;
+// export default ProjectSeo;
